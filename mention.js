78e866f4d3e6e3abc750386e6e01c813628b2d96
Efe Şener <efesenertr@gmail.com>
2023-04-18 12:07:35 +0300

    v2.1.1 - Revize Listeners
    After realizing that some listeners were functioning differently on Android devices, we made some technical changes to ensure the program runs smoothly on all devices, including Android.

--- mention.js
+++ mention.js
@@ -4,9 +4,8 @@ function(instance, context) {
 
     /*
     
-    İlk harf yazıldığında ilk harfe göre p elemnetleri oluşturulacak
-    Sonrasında data-classname attribute unun searchText i içermesine göre p elementlerin visibilitesi güncellenecek
-    Menü kapatılırsa bütün p elementleri remove edilecek.
+    ekrandan taşma riskini indirmek için acılan menünün x pozisoynu + menünün widthi, tarayıcı gneişliğine denk geliyor mu
+    diye kontrol edicez. Geliyorsa margin-left vericez.-
     
     */
 
@@ -98,8 +97,12 @@ function(instance, context) {
 
             if (event.key === 'ArrowDown') {
                 currentIndex = (currentIndex + 1) % menuOptions.length;
+                menuOptions[currentIndex].focus();
+                menuOptions[currentIndex].style.backgroundColor = instance.data.theme_color;
             } else if (event.key === 'ArrowUp') {
                 currentIndex = (currentIndex - 1 + menuOptions.length) % menuOptions.length;
+                menuOptions[currentIndex].focus();
+                menuOptions[currentIndex].style.backgroundColor = instance.data.theme_color;
             } else if (event.key === 'Enter') {
 
                 addMention(menuOptions[currentIndex].id);
@@ -132,13 +135,11 @@ function(instance, context) {
                 }
             } else {
                 currentIndex = 0;
-                menuOptions[currentIndex].style.backgroundColor = instance.data.theme_color;
                 return;
             }
 
-            menuOptions[currentIndex].focus();
 
-            menuOptions[currentIndex].style.backgroundColor = instance.data.theme_color;
+
         }
     };
 
@@ -170,12 +171,10 @@ function(instance, context) {
             userItem.style.width = '100%';
 
             userItem.setAttribute('id', uniqueElementId);
-            userItem.setAttribute('data-username', filteredUsers[i].toLowerCase().replaceAll(' ', ''));
-
+            userItem.setAttribute('data-username', filteredUsers[i].toLowerCase());
             userItem.setAttribute('tabindex', 0); // to enable keyboard interactions
 
             userItem.classList.add("mention-visible");
-
 
 
             userItem.onmouseover = function () {
@@ -192,7 +191,6 @@ function(instance, context) {
                 this.style.transition = "background-color 0.1s ease";
                 this.style.backgroundColor = "";
             }
-
 
             var userRow = document.createElement("div");
             userRow.style.height = "auto";
@@ -245,7 +243,7 @@ function(instance, context) {
     }
 
     var listenKeys = (e) => { // after the menu opened we listen all key inputs
-        console.log("the key is: ", e);
+
 
         let selection = window.getSelection(); // learn selection's position
 
@@ -268,14 +266,12 @@ function(instance, context) {
 
         // var menu = document.getElementById("userMentionMenu");
         var menu = instance.data.menu;
+
         // CHECKPOINT 1
-        if (menu !== null) { // if the menu is already open
-            var ps = menu.querySelectorAll('p[data-username*="' + searchText.replaceAll(' ', '') + '"]');
-            var psNot = menu.querySelectorAll('p:not([data-username*="' + searchText.replaceAll(' ', '') + '"])');
+        if (document.getElementById("userMentionMenu") !== null) { // if the menu is already open 
+            var ps = menu.querySelectorAll('p[data-username*="' + searchText + '"]');
+            var psNot = menu.querySelectorAll('p:not([data-username*="' + searchText + '"])');
 
-
-
-            // we're hiding all the p tags which doesn't contain the search text
 
 
 
@@ -283,6 +279,12 @@ function(instance, context) {
 
                 ps[i].style.display = "block";
                 ps[i].classList.add('mention-visible');
+
+                if (!i) {
+                    ps[i].style.backgroundColor = instance.data.theme_color;
+                } else {
+                    ps[i].style.backgroundColor = "none";
+                }
 
             }
 
@@ -306,7 +308,6 @@ function(instance, context) {
             }
 
         }
-
 
 
 
@@ -345,6 +346,7 @@ function(instance, context) {
 
         // if the searchText's number of character lower than limit, then all the p tags will be removed
         else if (childElements[1] !== undefined && instance.data.searchText.length <= 1) {
+
             childElements.forEach((child) => {
                 if (child.id !== 'mentionEmptyState') {
                     //child.parentNode.removeChild(child);
@@ -355,12 +357,12 @@ function(instance, context) {
 
         // if the menu is visible but search line doesn'T contain '@' character, we are removing the menu
 
-        if (!searchString.includes('@') && menu !== null) {
+       
+
+        if (!range.startContainer.textContent.substring(start-1, range.startOffset).includes('@') && menu !== null) {
             removeGroupFocus(menu);
 
         }
-
-
     }
 
     function removeGroupFocus(menu) {
@@ -385,7 +387,6 @@ function(instance, context) {
     }
 
     function openGroupFocus() {
-
 
         // creating "menu" element here
         var menu = document.createElement("div");
@@ -420,11 +421,11 @@ function(instance, context) {
         instance.data.emptyState = emptyState;
         instance.data.menu = menu;
 
+
         menu.addEventListener('click', itemSelectionCheck);
 
         document.addEventListener('click', clickedElementIsMenu);
 
-        console.log("robin");
         document.addEventListener('keydown', keyboardInteractions);
 
 
@@ -438,7 +439,6 @@ function(instance, context) {
 
         // richEditor.addEventListener("input", listenKeys(menu));
         richEditor.addEventListener("input", listenKeys);
-
 
     }
 
@@ -468,9 +468,6 @@ function(instance, context) {
 
         var richTextElement = document.querySelector(`#${el} > .ql-container > div.ql-editor`);
 
-
-        console.log(instance.data.userInput);
-        console.log(instance.data.searchText);
         richTextElement.innerHTML = richTextElement.innerHTML.replace("@" + instance.data.userInput, valueToBeReplaced);
 
         instance.triggerEvent('mention_added');
@@ -549,9 +546,23 @@ function(instance, context) {
     // LISTENER DEFINATIONS
 
 
+    document.addEventListener('input', function (event) {
 
+        if (event.data === '@') {
+            if (focused) {
+                if (wholeListUploaded && instance.data.menu !== null) {
+                    appendGroupFocus(instance.data.menu);
+                } else {
+                    openGroupFocus();
+                }
+            }
 
-    document.addEventListener('keydown', function (event) {
+        }
+    });
+
+    
+
+    document.addEventListener('keydown', function (event) { 
 
         if (event.key === '@') {
 
@@ -574,9 +585,4 @@ function(instance, context) {
     });
 
 
-
 }
-
-
-
-
